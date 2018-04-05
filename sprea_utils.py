41b57ca404be167061d5755126d677c183714189
Matteo Gaito <matteo.gaito@dada.eu>
2018-04-05 17:29:32 +0200

    Refactoring with Sprea Class

--- sprea_utils.py
+++ sprea_utils.py
@@ -1,53 +1,79 @@
 # -*- coding: utf-8 -*-
 
+#TODO
+# rifattorizzare con classi login in __init__
+
 import mechanicalsoup
+import os
 from urllib.parse import urlparse,parse_qs,urlencode
 
 URL = "http://sprea.it"
 
-def loginIntoSprea(user,password):
-    #logger.info('Starting get pdf data')
-    # Create a browser object
-    browser = mechanicalsoup.Browser()
-    # request Page
-    login_page = browser.get(URL)
-    # grab the login form
-    login_form = login_page.soup.find("form")
-    # insert user and password
-    login_form.find("input", {"id": "user"})["value"] = user
-    login_form.find("input", {"id": "password"})["value"] = password
-    response = browser.submit(login_form, login_page.url)
-    if response.status_code == 200:
-        #return logged browser
-        return(browser)
-    else:
-        print("error")
 
-def listCampaigns(user,password):
-    #Login to sprea.it
-    browser = loginIntoSprea(user,password)
-    campaigns_page = browser.get(URL + "/digitali/")
-    #Go to digital campaigns list page
-    campaigns_main = campaigns_page.soup.find("main", {"itemprop": "mainContentOfPage" })
-    campaigns_list = campaigns_main.find_all("div", {"class": "col-lg-2 col-md-4 col-xs-6" })
-    # print title and link
-    for campaign in campaigns_list:
-        elements = campaign.find('a')
-        print(elements['href'])
-        print(elements.find('strong').text)
+class Sprea(object):
+    def __init__(self, username, password):
+        self.URL = "http://sprea.it"
+        self.USERNAME = username
+        self.PASSWORD = password
 
-def CampaignBookView(user,password,campaign_url):
-    #Login to sprea.it
-    browser = loginIntoSprea(user,password)
-    campaign_books_view = browser.get( URL + campaign_url )
-    return(campaign_books_view)
+    def _login(self):
+        # logger.info('Starting get pdf data')
+        # Create a browser object
+        browser = mechanicalsoup.Browser()
+        # request Page
+        login_page = browser.get(URL)
+        # grab the login form
+        login_form = login_page.soup.find("form")
+        # insert user and password
+        login_form.find("input", {"id": "user"})["value"] = self.USERNAME
+        login_form.find("input", {"id": "password"})["value"] = self.PASSWORD
+        response = browser.submit(login_form, login_page.url)
+        if response.status_code == 200:
+            # return logged browser
+            return(browser)
+        else:
+            print("error")
 
-def downloadPDFofCampaign(campaign_books_view,book_position):
-    books_divs = campaign_books_view.soup.find_all("div", {"class": "col-lg-2 col-md-4 col-xs-6" })
-    pdf_url = books_divs[book_position].find('a')['href']
-    pdf_name = parse_qs((urlparse(pdf_url).query))['doc'][0]
-    anagrafica = parse_qs((urlparse(pdf_url).query))['a'][0]
-    pdf_title = parse_qs((urlparse(pdf_url).query))['o'][0]
-    print(pdf_name)
-    print(anagrafica)
-    print(pdf_title)
+    def listCampaigns(self):
+        # Login to sprea.it
+        browser = self._login()
+        campaigns_page = browser.get(self.URL + "/digitali/")
+        # Go to digital campaigns list page
+        campaigns_main = campaigns_page.soup.find(
+            "main",
+            {"itemprop": "mainContentOfPage"}
+        )
+        campaigns_list = campaigns_main.find_all(
+            "div",
+            {"class": "col-lg-2 col-md-4 col-xs-6"}
+        )
+        # print title and link
+        for campaign in campaigns_list:
+            elements = campaign.find('a')
+            print("{} {}".format(
+                elements['href'],
+                elements.find('strong').text)
+            )
+
+    def _goIntoCampaign(self, campaign_url):
+        browser = self._login()
+        campaign_books = browser.get(self.URL + campaign_url)
+        return(campaign_books)
+
+    def _manageDownloadDir(self, download_dir):
+        if not os.path.exists(download_dir):
+            os.makedirs(download_dir)
+
+    def downloadOnePdfOfCampaign(
+        self, campaign_url, book_position, download_dir="downloads"
+    ):
+        self._manageDownloadDir(download_dir)
+        browser = self._goIntoCampaign(campaign_url)
+        books_divs = browser.soup.find_all(
+            "div",
+            {"class": "col-lg-2 col-md-4 col-xs-6"}
+        )
+        pdf_url = books_divs[book_position].find('a')['href']
+        pdf_name = parse_qs((urlparse(pdf_url).query))['doc'][0]
+        anagrafica = parse_qs((urlparse(pdf_url).query))['a'][0]
+        pdf_title = parse_qs((urlparse(pdf_url).query))['o'][0]
